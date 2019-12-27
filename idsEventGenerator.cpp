1ab7114bc85cf23d56d18d12ca1f4f576f4ae6cf
druesendieb <druesendieb@users.noreply.github.com>
2019-12-27 22:06:04 +0100

    output change #2
    switched usedPort and Sid  for easier snort/suricata logfile regexing.

--- idsEventGenerator.cpp
+++ idsEventGenerator.cpp
@@ -1325,7 +1325,7 @@ void sendRulePacket(snortRule* rule, std::string host,bool verbose){
 	long usedPort;
 	result = curl_easy_getinfo(handle, CURLINFO_LOCAL_PORT, &usedPort);
 	if(result==CURLE_OK) {
-                printf("%ld:%s\n",usedPort,rule->body.sid.c_str());
+                printf("%s:%ld\n",rule->body.sid.c_str(),usedPort);
 	}else{
 		printf("Failure in getting local port\n");
 	}
